d45f157b3fa6a00161febb83f6e4655a458c0306
Nganga Kingori <ngangakelvin254@gmail.com>
2025-05-20 08:35:07 +0300

    practice, practice makes the interview cut.

--- src/practice/interviewQuestions/KSum.java
+++ src/practice/interviewQuestions/KSum.java
@@ -1,8 +1,6 @@
 package practice.interviewQuestions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class KSum {
     public static List<List<Integer>> threeSum(int[] nums){
